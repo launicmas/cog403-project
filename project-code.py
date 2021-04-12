dab6d01d573408592e0a3bf0550ad6004bedba24
Lauren Mason <l.mason@mail.utoronto.ca>
2021-04-12 15:27:02 -0400

    Added code for confusion matrices.

--- project-code.py
+++ project-code.py
@@ -1,8 +1,9 @@
 import json
+from typing import Dict, List
 import numpy as np
 from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import confusion_matrix
 from sklearn.model_selection import GridSearchCV, train_test_split
-from typing import Dict, List
 
 ALM = 0
 BALTIMORE = 1
@@ -63,7 +64,9 @@ def run_log_regression(X, Y):
     clf = GridSearchCV(log_reg, parameters)
     clf.fit(X_train, Y_train)
 
-    return clf.score(X_train, Y_train), clf.score(X_test, Y_test)
+    conf_m = confusion_matrix(Y_test, clf.predict(X_test))
+
+    return clf.score(X_train, Y_train), clf.score(X_test, Y_test), conf_m
 
 
 if __name__ == "__main__":
@@ -73,42 +76,48 @@ if __name__ == "__main__":
     # Group 1: All data
     all_X = np.concatenate((alm_X, baltimore_X, blm_X, davidson_X, election_X, metoo_X, sandy_X), axis=0)
     all_Y = np.concatenate((alm_Y, baltimore_Y, blm_Y, davidson_Y, election_Y, metoo_Y, sandy_Y))
-    train_acc, test_acc = run_log_regression(all_X, all_Y)
+    train_acc, test_acc, conf_m = run_log_regression(all_X, all_Y)
     print("Group 1 training accuracy: ", train_acc)
     print("Group 1 testing accuracy: ", test_acc)
+    print("Group 1 Confusion Matrix:\n", conf_m)
 
-    # Group 2: ALM and BLM only, because we are most confident in those labels
+    # Group 2: ALM and BLM only
     X = np.concatenate((alm_X, blm_X), axis=0)
     Y = np.concatenate((alm_Y, blm_Y))
-    train_acc, test_acc = run_log_regression(X, Y)
+    train_acc, test_acc, conf_m = run_log_regression(X, Y)
     print("Group 2 training accuracy: ", train_acc)
     print("Group 2 testing accuracy: ", test_acc)
+    print("Group 2 Confusion Matrix:\n", conf_m)
 
-    # Group 3: All data except Sandy and Elections, because we are least sure about those labels
+    # Group 3: All data except Sandy and Elections
     X = np.concatenate((alm_X, baltimore_X, blm_X, davidson_X, metoo_X), axis=0)
     Y = np.concatenate((alm_Y, baltimore_Y, blm_Y, davidson_Y, metoo_Y))
-    train_acc, test_acc = run_log_regression(X, Y)
+    train_acc, test_acc, conf_m = run_log_regression(X, Y)
     print("Group 3 training accuracy: ", train_acc)
     print("Group 3 testing accuracy: ", test_acc)
+    print("Group 3 Confusion Matrix:\n", conf_m)
 
     # Group 4: Permute Election and Sandy labels
     sandy_0 = np.zeros(sandy_Y.shape)
     election_1 = np.full(election_Y.shape, 1)
 
     Y = np.concatenate((alm_Y, baltimore_Y, blm_Y, davidson_Y, election_1, metoo_Y, sandy_Y))
-    train_acc, test_acc = run_log_regression(all_X, Y)
+    train_acc, test_acc, conf_m = run_log_regression(all_X, Y)
     print("Group 4 training accuracy (E=1, S=1): ", train_acc)
     print("Group 4 testing accuracy (E=1, S=1): ", test_acc)
+    print("Group 4 Confusion Matrix (E=1, S=1):\n", conf_m)
 
     Y = np.concatenate((alm_Y, baltimore_Y, blm_Y, davidson_Y, election_Y, metoo_Y, sandy_0))
-    train_acc, test_acc = run_log_regression(all_X, Y)
+    train_acc, test_acc, conf_m = run_log_regression(all_X, Y)
     print("Group 4 training accuracy (E=0, S=0): ", train_acc)
     print("Group 4 testing accuracy (E=0, S=0): ", test_acc)
+    print("Group 4 Confusion Matrix (E=0, S=0):\n", conf_m)
 
     Y = np.concatenate((alm_Y, baltimore_Y, blm_Y, davidson_Y, election_1, metoo_Y, sandy_0))
-    train_acc, test_acc = run_log_regression(all_X, Y)
+    train_acc, test_acc, conf_m = run_log_regression(all_X, Y)
     print("Group 4 training accuracy (E=1, S=0): ", train_acc)
     print("Group 4 testing accuracy (E=1, S=0): ", test_acc)
+    print("Group 4 Confusion Matrix (E=1, S=0):\n", conf_m)
 
 
     
